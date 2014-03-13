a9527d4eff48a7a35716ae6445afeee7c83e3bd7
Nelson Osacky <nelson@osacky.com>
2014-03-13 14:18:00 -0500

    cleaned up code

--- serialtest.py
+++ serialtest.py
@@ -9,19 +9,17 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(17, GPIO.IN)
 
 runningtotal = 0.0
-transactiontotal = 0.0
-pennies = 0
-nickels = 0
-dimes = 0
-quarters = 0
+totalpennies = 0
+totalnickels = 0
+totaldimes = 0
+totalquarters = 0
 
 def waitForButton():
-	global pennies
-	global nickels
-	global dimes
-	global quarters
-	global runningtotal
-	global transactiontotal
+	pennies = 0
+	nickels = 0
+	dimes = 0
+	quarters = 0
+	transactiontotal = 0.0
 	while (GPIO.input(17)):
 		coinval = port.read(1)
 		if (len(coinval) is 0):
@@ -31,29 +29,28 @@ def waitForButton():
 		if   coinval == 1:
 			print "You got a penny!"
 			pennies += 1
-			runningtotal += .01
 			transactiontotal += .01
 		elif coinval ==  5:
 			print "You got a nickel!"
 			nickels+= 1
-			runningtotal += .05
 			transactiontotal += .05
 		elif coinval == 10:
 			print "You got a dime!"
 			dimes+= 1
-			runningtotal += .10
 			transactiontotal += .10
 		elif coinval == 25:
 			print "That was a quarter!"
 			quarters+= 25
-			runningtotal += .25
 			transactiontotal += .25
 	print "button was pushed"
+	return (transactiontotal, pennies, nickels, dimes, quarters)
 
 while True:
 	print "put some money in"
-	waitForButton();
-	print transactiontotal
-	print pennies
+	transaction = waitForButton();
+	print "before runningtotal is", runningtotal, "pennies total", totalpennies
+	runningtotal += transaction[0]
+	totalpennies += transaction[1]
+	print "runningtotal is", runningtotal, "pennies total", totalpennies
 	time.sleep(5) #this is where we print
 	transactiontotal = 0.
